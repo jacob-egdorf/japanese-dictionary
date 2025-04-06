0658f57f83788dd983a08fdce46ee3fe253d1f2c
jacob-egdorf <jcegdorf@gmail.com>
2025-04-05 22:37:24 -0400

    refactor: use collection.insert_many() to seed database directly from JMdict xml
    Using collection.insert_many() to insert a dict containing all entries is much faster than using collection.insert_one() to insert individual entries.
    
    Refactor init_dictionary.py to transform the JMdict file to a dict from XML using xmltodict, rather than writing custom processing per field. Perform some minor postprocessing to preserve types, remove special characters from dict keys, and force elements which may contain multiple values to be converted to lists.

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("search/", views.search, name="search"),
-    path("word/<entry_id>", views.word, name="word")
+    path("word/<ent_seq>", views.word, name="word")
 ]
